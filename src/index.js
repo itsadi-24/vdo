039e7d8a8cf38f936f9eb051c6994b6e00f85ace
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-03-28 20:49:43 +0530

    utility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // require("dotenv").config({path:"/.env"});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
 
 dotenv.config({ path: "/.env" });
 
-connectDB();
+const app = express();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed: ", error);
+  });
 
 /*
 one way of connecting to MongoDB 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+
+    if (statck) {
+      this.statck = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
